180b02eee317904e49bffbb7a858858af04d55ed
imgesu <icetin@ucsd.edu>
2020-09-16 09:40:45 +0300

    updated by imgesu

--- Application Files: September/App_IOM_expanded.R
+++ Application Files: September/App_IOM_expanded.R
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
 
-
-# Source ------------------------------------------------------------------
-source("Functions_RShiny.R")
-source("Functions_RShiny.R")
-
-
-# User Interface ----------------------------------------------------------
-
-ui<-fluidPage(
-    
-    titlePanel("Google Trends Index"),
-    sidebarLayout(
-        sidebarPanel(width = 3,
-                     
-                     # selectInput("origin_code", label = h5("Origin Country"),
-                     #             choices = list("SY", "VE"), selected = "SY"),
-                     selectInput("origin_code", label = h5("Origin Country"),
-                                 choices = Countries_gtrends$country_name, selected = "SYRIA"),
-                     
-                     selectInput("search_terms", label = h5("Serach key word"),
-                                 choices=unique(Migrationdata$Country),selected = "Germany"),
-                     
-                     selectInput("lang", label=h5("language"),
-                                 choices= list('ab','en','fr','es'),selected = "en"),
-                     
-                     selectInput("origin", label = h5("Origin of Migrants"),
-                                 choices=unique(Migrationdata$Country.of.birth.nationality),selected = "Syria" ),
-                     
-                     selectInput("destination", label = h5("Destination of Migrants"),
-                                 choices=unique(Migrationdata$Country),selected = "Germany" ),
-                     #______ Migration keywords for top searches -------#
-                     
-                     selectInput("migration.keyword", label = h5("Select Keyword for top hits globaly"),
-                                 choices=c("migration","asylum"),selected = "migration"),
-                     
-                     #______ input to select for top hits around the world in regards to time-------#
-=======
 # Source -------------------------------------------------------------------
 source("Functions_Rshiny_Imgesu.R")
 source("Functions_RShiny_Murtiza.R")
@@ -70,21 +32,27 @@ ui<-fluidPage(theme = shinytheme("cerulean"),
                       #             choices=unique(Migrationdata$Country.of.birth.nationality),selected = "Syria" ),
                       # 
                       #______ Migration keywords for top searches -------#
+                      helpText("Options needed for second and third tab"),
+                      
                       
                       selectInput("migration.keyword", label = h5("Select Keyword for top hits globaly"),
                                   choices=c("migration","asylum"),selected = "migration"),
                       
                       #______ input to select for top hits around the world in regards to time-------#
->>>>>>> 14cc3dc08b3bb1abb4a0d8ae6d04d91b00e3c980
                      
                      selectInput("time.map", label = h5("Select time for top hits globally "),
                                  choices = c("now 1-H",
-                                             "now 4-H"),
+                                             "now 4-H",
+                                             "now 1-d",
+                                             "now 7-d",
+                                             "today 1-m",
+                                             "today 3-m",),
                                  selected = "now 4-H")),
         
         #____________________________main panel output ____________________# 
         mainPanel( tabsetPanel(type = "tabs",
-                               tabPanel("Plots", plotOutput("plot1",height=700)),
+                               tabPanel("Specification Plots", plotOutput("plot1",height=700),dataTableOutput("similarity.table"),textOutput("selected_var")),
+                               tabPanel("Prediction", plotOutput("predict",height = 700)),
                                tabPanel("Top Hits Map",plotOutput("map")),
                                tabPanel("Top Hits table",dataTableOutput("mytable")))
                    
@@ -99,25 +67,11 @@ ui<-fluidPage(theme = shinytheme("cerulean"),
 # Server ------------------------------------------------------------------
 
 server<-function(input, output) {
-    # data("countries")
-    
+
+    #____________________first tab for ploting all specifications_________
     dataInput <- reactive({
         Dashboard_DT(Migrationdata,
                      asylum_applications_UNHCR,
-<<<<<<< HEAD
-                     origin = input$origin,
-                     destination = input$destination,
-                     Year_from = 2005,
-                     Year_till = 2018,
-                     origin_asylum = input$origin,
-                     destination_asylum = input$destination,
-                     serach_terms = input$search_terms,
-                     origin_code = input$origin_code,
-                     language = input$lang,
-                     category = input$category)
-    })
-    #plots output 1
-=======
                      origin = input$geo,
                      destination = input$destination,
                      Year_from = 2005,
@@ -129,23 +83,47 @@ server<-function(input, output) {
                      language = "en",
                      category = "Immigration Policy & Border Issues")})
     
-    #this uses the first original function
-    # Generate_comparison(serach_terms = input$queries,
-    #                      origin_code = input$geo,
-    #                       language = input$lang,
-    #                      origin = input$Migration.origin,
-    #                      destination = input$Migration.destination,
-    #                     origin_asylum = input$Asylum.origin,
-    #                     destination_asylum = input$Asylum.destination)
-    #plots our function of 12 plots
->>>>>>> 14cc3dc08b3bb1abb4a0d8ae6d04d91b00e3c980
+
+    #_____plots our function of 12 plots____
+
     output$plot1<- renderPlot({
         
         Dashboard_plot(dataInput())
-        
-        
+ })
+    output$selected_var <- renderText({ 
+        paste("Approach 1: Destination as keywords, category as Visa and Immigration for origin", input$geo, "to", input$destination)
+        paste("Approach 2: Destination as keywords, without category for origin", input$geo, "to", input$destination)
+        paste("Aprroach 3: Category as 555 Without any keywords for origin", input$geo, "to", input$destination)
         
     })
+    #_____similarity score at the bottom of first specicification tab______
+    
+    output$similarity.table<- renderDataTable({
+        #insert table for similartiy score
+    })
+    
+    #create reactive data for prediction
+    predict.data <- reactive({
+        Dashboard_DT_without_norm(Migrationdata,
+                                  asylum_applications_UNHCR,
+                                  origin = input$geo,
+                                  destination = input$destination,
+                                  Year_from = 2005,
+                                  Year_till = 2018,
+                                  origin_asylum = input$geo,
+                                  destination_asylum = input$destination,
+                                  serach_terms = input$destination,
+                                  origin_code = input$geo,
+                                  language = "en",
+                                  category = "Immigration Policy & Border Issues")
+        
+    })
+    #______________second tab for prediction_____________________________
+    output$predict <- renderPlot({
+        #put function here to render plot
+        Predicting_migration(predict.data)
+    })
+    #__________________third tab to render map of top hits____________________
     output$map <- renderPlot({
         a<-top.hits(world,
                     search_keyword = input$migration.keyword,
@@ -153,6 +131,8 @@ server<-function(input, output) {
         
         top.hits.map(a)
     })
+    #_________________fourth tab to show data table of top hits___________________
+    
     data<- reactive({
         gtrends(keyword = input$migration.keyword,
                 time = input$time.map)$interest_by_country%>%
@@ -166,11 +146,12 @@ server<-function(input, output) {
         
     })
     
-<<<<<<< HEAD
+    
+
 }#end server
-=======
-}#ends server function
->>>>>>> 14cc3dc08b3bb1abb4a0d8ae6d04d91b00e3c980
+
+
+
 
 # Run ---------------------------------------------------------------------
 shinyApp(ui,server)
